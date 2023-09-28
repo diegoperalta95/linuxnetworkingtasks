22d750fbf19b63a07195e246655cb8da64cde6f4
Diego Peralta <diegooperalta95@gmail.com>
2023-09-27 23:06:44 -0300

    Vim auto-identing gg=G

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 
 
 function charCheck (){
-    local input=$1
-    local type=$2
+	local input=$1
+	local type=$2
 
 	if [ "$type" == "letter" ] && [[ ! "$input" =~ ^[a-zA-Z]+$ ]]; then
 		echo " Only letters are allowed."
@@ -24,35 +24,35 @@ function createContact (){
 
 	printf "\n"
 	read -r -p " Insert a name: " name
-    while ! charCheck "$name" "letter"; do
-        read -r -p " Insert a valid name: " name
-    done
-    
+	while ! charCheck "$name" "letter"; do
+		read -r -p " Insert a valid name: " name
+	done
+
 	read -r -p " Insert a last name: " lastName
-    while ! charCheck "$lastName" "letter"; do
-        read -r -p " Insert a valid last name: " lastName
-    done
+	while ! charCheck "$lastName" "letter"; do
+		read -r -p " Insert a valid last name: " lastName
+	done
 
 	if ! charCheck "$lastName" "letter" ; then exit; fi
 
 	read -r -p " Insert email: " email
-    while ! charCheck "$email" "email"; do
-        read -r -p " Insert a valid email: " email
-    done
-    
+	while ! charCheck "$email" "email"; do
+		read -r -p " Insert a valid email: " email
+	done
+
 	read -r -p " Insert phone: " phone
 	while ! charCheck "$phone" "phone"; do
-        read -r -p " Insert a valid phone number: " phone
-    done
+		read -r -p " Insert a valid phone number: " phone
+	done
 
 
 	local contactInfo="$name $lastName $email $phone"
 	if echo "$contactInfo" >> contacts.txt; then
 		printf "\n Contact added!\n"
-        return 0
+		return 0
 	else
 		printf " Failed to add the contact\n"
-        return 1
+		return 1
 	fi
 }
 
@@ -66,21 +66,21 @@ function searchContact (){
 function help (){
 	printf "\nHelp Menu
 
-Menu Navigation:
-Enter the number option followed by an enter, to select any option
+	Menu Navigation:
+	Enter the number option followed by an enter, to select any option
 
-Creating a new contact:
-To create a new contact, select the first option of the menu with the number 1
-Now, type the data required in order: name, last name, email and phone numb
+	Creating a new contact:
+	To create a new contact, select the first option of the menu with the number 1
+	Now, type the data required in order: name, last name, email and phone numb
 
-Search for a contact:
-The user can search any previous contact saved, by entering the second option of the menu with the number 2
-Once inside the second option, just type the word/number you want to search followed by an enter
-"
+	Search for a contact:
+	The user can search any previous contact saved, by entering the second option of the menu with the number 2
+	Once inside the second option, just type the word/number you want to search followed by an enter
+	"
 }
 
 function mainMenu (){
-    local menu
+	local menu
 
 	while [ "$menu" != 'ex' ]; do
 		printf "\n"
@@ -96,7 +96,7 @@ function mainMenu (){
 			2) searchContact;;
 			3) help;;
 			4) printf "Bye!\n"
-		       menu="ex";;
+				menu="ex";;
 			*) printf "\n Not a valid option \n"
 		esac
 	done
